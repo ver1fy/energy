818a2111e7265d4f6957f33500dad8c8866ffb78
Tom <tom-1@mail.com>
2023-03-01 13:28:08 +0000

    chore: added webpack JS loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,16 @@ module.exports = {
           'sass-loader'
         ]
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ]
   },
   plugins: [
